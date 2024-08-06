a6df5369603bf68ebb97135e43fe6dc0375b55e4
Lucas <lucaspk91@gmail.com>
2024-08-06 16:33:48 -0300

    chore: add seed script

---
+++ prisma/migrations/20240806192953_init_db/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image` on the `Estetica` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Estetica" DROP COLUMN "image";
